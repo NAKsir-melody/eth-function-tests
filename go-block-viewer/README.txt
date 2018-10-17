6bb96c87b79574233942615b4b850a98192cfbe4
sigmoid <alphabet@hotmail.co.kr>
2018-10-17 11:52:57 +0900

    Add simple block scanner
    Signed-off-by: sigmoid <alphabet@hotmail.co.kr>

---
+++ go-block-viewer/README.txt
@@ -0,0 +1,28 @@
+Scan ethereum chain with Go.
+
+
+#prepare
+Select network in code(comment/uncomment)
+
+
+#run
+go run main.go
+
+
+you can see the block and trasaction information when new block generated
+
+6529434
+==============
+0 0xc69cbce63b904a36ee9f37c2aad62618d5bf95c91019dee16b0cd595605e6c33
+from:  0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8
+to:  0x2ee5563cC2562eaF3E346471C5A4d747a9b9bDc9
+value:  110497593392048986
+data:  []
+==============
+1 0xb2bc002e16924007c193d0907608da8619d2fe6364f7ed564faf83314b4ec6c9
+from:  0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8
+to:  0x521d823C9FC0539689FA2789490FBff591d64d30
+value:  50035998310763542
+data:  []
+==============
+
---
+++ go-block-viewer/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ScanBlock()
+}
+// major
+func ScanBlock() {
+	ctx := context.Background();
+	// main net
+	client, err := ethclient.DialContext(ctx, "https://mainnet.infura.io/v3/d023760061574ff7b403904d70ee0d55")
+
+	// test net
+	//client, err := ethclient.DialContext(ctx, "https://ropsten.infura.io/v3/d023760061574ff7b403904d70ee0d55")
+
+	// private net
+	//client, err := ethclient.DialContext(ctx, "http://127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("rpc conn error")
+		return
+	}
+	defer client.Close()
+
+	var nBlock uint64
+	nBlock = 0
+	for ; ; {
+		//b_num := big.NewInt(1)
+		//block, err := client.BlockByNumber(ctx,b_num)
+		block, err := client.BlockByNumber(ctx,nil)
+		if err != nil {
+			fmt.Println("block get failed")
+			return
+		}
+		bn := block.NumberU64();
+		if(nBlock != bn) {
+			nBlock = bn;
+			fmt.Println(nBlock)
+			//fmt.Println(block.Time())
+			//fmt.Println(block.Difficulty())
+			//block_hash := block.Hash()
+			block_body := block.Body()
+
+			for no, tx := range block_body.Transactions {
+				fmt.Println("==============")
+				fmt.Println(no ,tx.Hash().Hex())
+				from, err := client.TransactionSender(ctx, tx, block.Hash(),uint(no))
+				if(err == nil){
+					fmt.Println("from: ",from.Hex())
+				}
+				fmt.Println("to: ",tx.To().Hex())
+				result := tx.Value()
+				fmt.Println("value: ",result)
+				fmt.Println("data: ",tx.Data())
+				time.Sleep(50 * time.Millisecond)
+			}
+		}
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+}
+
