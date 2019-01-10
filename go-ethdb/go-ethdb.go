1d505a12612a2e31a5958a2e2ef1b8728c8556b0
sigmoid <alphabet@hotmail.co.kr>
2019-01-10 18:59:18 +0900

    go-ethdb
    db open & read all
    
    Signed-off-by: sigmoid <alphabet@hotmail.co.kr>

---
+++ go-ethdb/go-ethdb.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethdb"
+)
+
+func main() {
+	ldb, err := ethdb.NewLDBDatabase("chaindata", 0, 0);
+	if err != nil {
+		fmt.Printf("open error\n" + err.Error())
+		return
+	}
+	iter := ldb.NewIterator()
+	for iter.Next() {
+		key := iter.Key()
+		value := iter.Value()
+		fmt.Printf("[%x, %x]", key, value)
+		fmt.Printf("\n")
+	}
+	iter.Release()
+	err = iter.Error()
+	if err != nil {
+		fmt.Printf("iter error\n" + err.Error())
+		return
+	}
+}
+
